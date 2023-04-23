bffbbe59ed616d7358560d5514783025e01d9a58
Kate <ktvlphoto@outlook.com>
2023-04-23 10:59:35 +0300

    Base Test class

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -3,21 +3,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import runner.BaseTest;
 
-public class GoogleTest {
+public class GoogleTest extends BaseTest {
 
     @Test
     public void testSelenium() throws InterruptedException {
-        WebDriver driver = new ChromeDriver(); // открываем браузер
-        driver.get("https://www.google.com/");
-        WebElement textbox = driver.findElement(By.name("q"));   //найти поле куда вписать текст
+
+        getDriver().get("https://www.google.com/");
+        WebElement textbox = getDriver().findElement(By.name("q"));   //найти поле куда вписать текст
         textbox.sendKeys("selenium\n"); //отправлять какие-то данные в этот элемент
         Thread.sleep(2000);
 //        WebElement button = driver.findElement(By.name("btnK")); // находим кнопку
 //        button.click();
-        WebElement text = driver.findElement(By.xpath("//h3[text()='Selenium']"));
+        WebElement text = getDriver().findElement(By.xpath("//h3[text()='Selenium']"));
         Assert.assertEquals(text.getText(), "Selenium"); // сравниваем через тестнж
-        driver.quit(); // закрываем браузер
+
     }
 }
